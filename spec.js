ec8b6b13de86f614a8af45fea0d5ef1f275efeb7
prof-katz <eric.katz@fullstackacademy.com>
2021-07-30 13:23:09 -0400

    done

--- spec.js
+++ spec.js
@@ -1,3 +1,19 @@
+const Sequelize = require('sequelize');
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_widgets_db');
+
+const Widget = conn.define('widget', {
+});
+
+const syncAndSeed = async()=> {
+  await conn.sync({ force: true });
+  await Promise.all([
+    Widget.create(),
+    Widget.create(),
+    Widget.create(),
+    Widget.create(),
+
+  ]);
+};
 const { expect } = require('chai');
 const fizzBuzz = (len)=> {
   const result = [];
@@ -9,5 +25,12 @@ const fizzBuzz = (len)=> {
 describe('fizz buzz', ()=> {
   it('works', ()=> {
     expect(fizzBuzz(2)).to.eql([ 1, 2]);
+  });
+});
+describe('widgets', ()=> {
+  beforeEach(()=> syncAndSeed());
+  it('there are 4', async()=> {
+    const widgets = await Widget.findAll();
+    expect(widgets.length).to.equal(4);
   });
 });
